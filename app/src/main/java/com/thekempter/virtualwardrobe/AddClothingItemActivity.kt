72d3664df068bae9b1154dbbf85184b818bdb792
DerKempter <Joshkempter@gmail.com>
2023-04-15 15:29:15 +0200

    Modify AddClothingItem Activity
    It now makes the image smaller when scrolling
    It is now possible to directly take a picture

--- app/src/main/java/com/thekempter/virtualwardrobe/AddClothingItemActivity.kt
+++ app/src/main/java/com/thekempter/virtualwardrobe/AddClothingItemActivity.kt
@@ -1,14 +1,21 @@
 package com.thekempter.virtualwardrobe
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -31,6 +38,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
@@ -64,15 +72,33 @@ class AddClothingItemActivity : ComponentActivity() {
         material: MutableState<String>,
         imageUrl: MutableState<String>,
         ) {
+        val scrollState = rememberScrollState()
+        val context = LocalContext.current
 
-        val launcherWithPrevImage = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()){
+        val launcherGallery = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()){
                 uri ->
             if(uri.toString() == "null"){
                 imageUrl.value = imageUrl.value
             }
             else{
                 imageUrl.value = uri.toString()
-                name.value = uri.toString()
+            }
+        }
+
+        val imageUri: Uri = remember {
+            val contentValues = ContentValues().apply {
+                put(MediaStore.Images.Media.DISPLAY_NAME, "picture.jpg")
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            }
+            // Insert a new empty image into the MediaStore
+            val contentResolver = context.contentResolver
+            contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)!!
+            // TODO make it so image is not saved in gallery aka find a way for the picture to not appear in gallery
+        }
+        val launcherCamera = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            if (success) {
+                // Update the imageUri with the new picture taken
+                imageUrl.value = imageUri.toString()
             }
         }
 
@@ -80,98 +106,105 @@ class AddClothingItemActivity : ComponentActivity() {
         VirtualWardrobeTheme{
             Scaffold(
                 content = {
-                    Column(
+                    Box(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(16.dp)
-                            .verticalScroll(rememberScrollState()),
-                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        if (imageUrl.value.isNotEmpty()){
                             Image(
                                 painter = rememberAsyncImagePainter(model = imageUrl.value),
                                 contentDescription = name.value,
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .height(400.dp)
-                                    .clip(shape = RoundedCornerShape(4.dp))
+                                    .align(Alignment.TopCenter)
+                                    .height((400 - scrollState.value).coerceAtLeast(150).dp)
+                                    .clip(shape = RoundedCornerShape(8.dp))
                                     .clickable {
-                                        launcherWithPrevImage.launch(
-                                            PickVisualMediaRequest(
-                                                mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
-                                            )
-                                        )
+                                        val options =
+                                            arrayOf("Take Photo", "Choose from Gallery", "Cancel")
+                                        val builder = AlertDialog.Builder(context)
+                                        builder.setTitle("Select an Option")
+                                        builder.setItems(options) { dialog, which ->
+                                            when (which) {
+                                                0 -> {
+                                                    launcherCamera.launch(imageUri)
+                                                }
+                                                1 -> {
+                                                    launcherGallery.launch(
+                                                        PickVisualMediaRequest(
+                                                            mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
+                                                        )
+                                                    )
+                                                }
+                                                else -> dialog.dismiss()
+                                            }
+                                        }
+                                        builder.show()
                                     },
                                 contentScale = ContentScale.Crop
                             )
+                        Column(
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .padding(bottom = 40.dp)
+                                .padding(top = (400 - scrollState.value).coerceAtLeast(150).dp)
+                                .verticalScroll(scrollState)
+                                .fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            OutlinedTextField(
+                                value = name.value,
+                                onValueChange = { name.value = it },
+                                label = { Text("Name") },
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            OutlinedTextField(
+                                value = type.value,
+                                onValueChange = { type.value = it },
+                                label = { Text("Type") },
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            OutlinedTextField(
+                                value = color.value,
+                                onValueChange = { color.value = it },
+                                label = { Text("Color") },
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            OutlinedTextField(
+                                value = brand.value,
+                                onValueChange = { brand.value = it },
+                                label = { Text("Brand") },
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            OutlinedTextField(
+                                value = size.value,
+                                onValueChange = { size.value = it },
+                                label = { Text("Size") },
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            OutlinedTextField(
+                                value = material.value,
+                                onValueChange = { material.value = it },
+                                label = { Text("Material") },
+                                modifier = Modifier.fillMaxWidth()
+                            )
                         }
-                        else{
-                            Image(
-                                painter = rememberAsyncImagePainter(model = ""),
-                                contentDescription = name.value,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(400.dp)
-                                    .clip(shape = RoundedCornerShape(4.dp))
-                                    .clickable {
-                                        launcherWithPrevImage.launch(
-                                            PickVisualMediaRequest(
-                                                mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
-                                            )
-                                        )
-                                    },
-                                contentScale = ContentScale.Crop,
-
-                                )
-                        }
-                        OutlinedTextField(
-                            value = name.value,
-                            onValueChange = { name.value = it },
-                            label = { Text("Name") },
-                            modifier = Modifier.fillMaxWidth()
+                        Spacer(
+                            modifier = Modifier.height(32.dp)
                         )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        OutlinedTextField(
-                            value = type.value,
-                            onValueChange = { type.value = it },
-                            label = { Text("Type") },
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        OutlinedTextField(
-                            value = color.value,
-                            onValueChange = { color.value = it },
-                            label = { Text("Color") },
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        OutlinedTextField(
-                            value = brand.value,
-                            onValueChange = { brand.value = it },
-                            label = { Text("Brand") },
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        OutlinedTextField(
-                            value = size.value,
-                            onValueChange = { size.value = it },
-                            label = { Text("Size") },
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        OutlinedTextField(
-                            value = material.value,
-                            onValueChange = { material.value = it },
-                            label = { Text("Material") },
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                        Spacer(modifier = Modifier.height(32.dp))
                         Button(
                             onClick = {
                                 // Save the data and go back to previous screen
                                 //onBackPressed()
                             },
-                            modifier = Modifier.fillMaxWidth()
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .align(Alignment.BottomCenter)
                         ) {
                             Text("Save")
                         }
