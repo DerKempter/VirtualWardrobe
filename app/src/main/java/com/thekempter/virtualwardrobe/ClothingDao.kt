ca390093350ba434b490f2e4281b8cdfaffcae78
DerKempter <Joshkempter@gmail.com>
2023-04-15 15:56:10 +0200

    modify clothingDao to support junctionTableQueries

--- app/src/main/java/com/thekempter/virtualwardrobe/ClothingDao.kt
+++ app/src/main/java/com/thekempter/virtualwardrobe/ClothingDao.kt
@@ -3,6 +3,7 @@ package com.thekempter.virtualwardrobe
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 
@@ -22,4 +23,13 @@ interface ClothingDao {
 
     @Query("SELECT * FROM clothing_items WHERE type = :type")
     fun getItemsByType(type: String): List<ClothingItem>
+
+    @Query("SELECT * FROM clothing_item_season WHERE clothingItemId = :clothingItemId")
+    fun getSeasonsForClothingItem(clothingItemId: Int): List<ClothingItemSeasonCrossRef>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertClothingSeasonJunction(junction: ClothingItemSeasonCrossRef)
+
+    @Delete
+    fun deleteClothingSeasonJunction(junction: ClothingItemSeasonCrossRef)
 }
