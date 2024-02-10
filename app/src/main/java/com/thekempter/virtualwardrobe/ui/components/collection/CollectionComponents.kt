4b0eadbbf2b6017e64d4bd3544fdf0a6f1994837
DerKempter <jkempter@students.uni-mainz.de>
2024-02-10 01:20:43 +0100

    Column -> LazyColumn

--- app/src/main/java/com/thekempter/virtualwardrobe/ui/components/collection/CollectionComponents.kt
+++ app/src/main/java/com/thekempter/virtualwardrobe/ui/components/collection/CollectionComponents.kt
@@ -1,25 +1,30 @@
 package com.thekempter.virtualwardrobe.ui.components.collection
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.thekempter.virtualwardrobe.ClothingViewModel
 import com.thekempter.virtualwardrobe.data.util.getClothingTypeForClothing
 import com.thekempter.virtualwardrobe.data.util.getSeasonsForClothing
 import com.thekempter.virtualwardrobe.ui.components.ClothingItemDisplay
 
 @Composable
-fun CollectionScreen(clothingViewModel: ClothingViewModel){
+fun CollectionScreen(clothingViewModel: ClothingViewModel, navController: NavController){
     val state by clothingViewModel.state.collectAsState()
     val allClothes = state.clothes
 
-    Column {
-        allClothes.forEach { clothingItem ->
+    LazyColumn {
+        items(items = allClothes, key = {it.id}) { clothingItem ->
             val seasons = getSeasonsForClothing(clothingViewModel, clothingItem.id)
             val clothingType = getClothingTypeForClothing(state, clothingItem.typeId)
 
-            ClothingItemDisplay(clothingItem, seasons, clothingType, clothingViewModel)
+            ClothingItemDisplay(clothingItem, seasons, clothingType, clothingViewModel, navController)
         }
     }
 }
