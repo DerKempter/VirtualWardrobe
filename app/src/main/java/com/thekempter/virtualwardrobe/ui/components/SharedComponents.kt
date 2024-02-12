e94c7845e27a1e2628fa9c7fe08bab88926f394f
DerKempter <jkempter@students.uni-mainz.de>
2024-02-12 18:26:55 +0100

    Make collection Box -> Card

--- app/src/main/java/com/thekempter/virtualwardrobe/ui/components/SharedComponents.kt
+++ app/src/main/java/com/thekempter/virtualwardrobe/ui/components/SharedComponents.kt
@@ -5,26 +5,23 @@ import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -33,6 +30,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -46,7 +44,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.PopupProperties
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -70,14 +67,34 @@ fun ClothingItemDisplay(
     navController: NavController
 ) {
     seasons.map { it.name }.distinct()
-    Box(modifier = Modifier.fillMaxWidth()){
-        Surface(modifier = Modifier.fillMaxWidth(),
-            onClick = {
-                clothingViewModel.currentClothingItem = clothingItem
-                navController.navigate("itemDetail/${clothingItem.id}")
+    Card(
+        modifier= Modifier
+            .padding(horizontal = 8.dp)
+            .padding(4.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background),
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.contentColorFor(MaterialTheme.colorScheme.background)),
+        onClick = {
+            clothingViewModel.currentClothingItem = clothingItem
+            navController.navigate("itemDetail/${clothingItem.id}")
+        }
+    ){
+        val padding = 12.dp
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+            Log.d("ClothingItemDisplay", "imageUrl = ${clothingItem.imageId}")
+            val clothingImageList = getImageForClothingById(clothingViewModel, clothingItem.imageId)
+            Column(
+                modifier = Modifier.fillMaxHeight().padding(padding),
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                if(clothingImageList.value.size == 1){
+                    LoadImageFromUriWithCoil(imageData = clothingImageList.value.first().bitData)
+                }
             }
-        ) {
-            Column(modifier = Modifier.padding(16.dp)) {
+            Column(modifier = Modifier.fillMaxHeight().padding(padding)) {
                 Text(
                     modifier = Modifier.align(Alignment.CenterHorizontally),
                     text = clothingItem.name,
@@ -105,11 +122,6 @@ fun ClothingItemDisplay(
                     }
                 }
             }
-        }
-        Log.d("ClothingItemDisplay", "imageUrl = ${clothingItem.imageId}")
-        val clothingImageList = getImageForClothingById(clothingViewModel, clothingItem.imageId)
-        if(clothingImageList.value.size == 1){
-            LoadImageFromUriWithCoil(imageData = clothingImageList.value.first().bitData)
         }
     }
 }
@@ -330,7 +342,6 @@ fun LoadImageFromUriWithCoil(imageData: ByteArray, size: Int = 300) {
             inSampleSize = scaleFactor
         })
     }
-
     AsyncImage(
         model = ImageRequest.Builder(context).data(bitmap).crossfade(true).build(),
         contentDescription = null,
